94263a4f1f16607a67ce69228fa5b0c005e2dd4c
NicolasMenesesB <NicolasMenesesB>
2022-08-27 13:38:31 -0400

    Creacion de razor page: Login

---
+++ app_CivilProcesses/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace app_CivilProcesses.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
